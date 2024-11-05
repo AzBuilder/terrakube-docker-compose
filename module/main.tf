8f78c04241a4f973d7501d43093f445d41df420d
Alfredo España <alfespa17@gmail.com>
2024-11-05 13:43:45 -0600

    test folder structure

--- module/main.tf
+++ module/main.tf
@@ -3,6 +3,10 @@ resource "random_integer" "time" {
   max = 5
 }
 
+module "time_module" {
+  source = "../submodule/module1"
+}
+
 output "output_time" {
   value = random_integer.time.result
 }
--- submodule/module1/main.tf
+++ submodule/module1/main.tf
@@ -3,10 +3,6 @@ resource "random_integer" "time" {
   max = 5
 }
 
-module "time_module" {
-  source = "../../module"
-}
-
 output "output_time" {
   value = random_integer.time.result
 }
